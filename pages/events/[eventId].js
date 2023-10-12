cd014c4043c7078a0d282fc27d7352548010e240
Leslie Chou <chouyuchien@gmail.com>
2023-10-11 21:11:43 -0700

    set up main page

---
+++ pages/events/[eventId].js
@@ -0,0 +1,14 @@
+
+
+function EventDetailPage() {
+    return (
+        <>
+        <div>
+            <h1>Event Detail</h1>
+        </div>
+        </>
+    )
+}
+
+
+export default EventDetailPage;
