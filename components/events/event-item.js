c60801e6e7fe56ad4733735a89768f1d312c381f
Leslie Chou <chouyuchien@gmail.com>
2023-10-12 14:53:31 -0700

    styled components

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
+import classes from './event-item.module.css'
 
 function EventItem(props) {
-    console.log('props in event-item component: ', props);
+    // console.log('props in event-item component: ', props);
     const { title, image, date, location, id} = props;
 
     const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
@@ -15,23 +16,23 @@ function EventItem(props) {
 
     return (
         <>
-        <li>
+        <li className={classes.item}>
             <img src={'/' + image} alt={title} />
+            <div className={classes.content}>
+                <div className={classes.summary}>
+                    <h2>{title}</h2>
+                    <div className={classes.date}>
+                        <time>{humanReadableDate}</time>
+                    </div>
+                    <div className={classes.address}>
+                        <address>{formattedAddress}</address>
+                    </div>
+                </div>
+                <div className={classes.actions}>
+                    <Link href={explorLink}>Explor Event</Link>
+                </div>
+            </div>
         </li>
-        <div>
-            <div>
-                <h2>{title}</h2>
-                <div>
-                    <time>{humanReadableDate}</time>
-                </div>
-                <div>
-                    <address>{formattedAddress}</address>
-                </div>
-            </div>
-            <div>
-                <Link href={explorLink}>Explor Event</Link>
-            </div>
-        </div>
         </>
     )
 }
