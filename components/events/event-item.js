5af2a6528c6fdc117c11ede08dae37f2049dc96a
Leslie Chou <chouyuchien@gmail.com>
2023-10-12 15:31:25 -0700

    added button and icons

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,8 +1,11 @@
-import Link from "next/link";
-import classes from './event-item.module.css'
+import Button from "../ui/button";
+import DateIcon from '../icons/date-icon';
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+import classes from './event-item.module.css';
 
 function EventItem(props) {
-    // console.log('props in event-item component: ', props);
+    console.log('props in event-item component: ', props);
     const { title, image, date, location, id} = props;
 
     const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
@@ -22,14 +25,21 @@ function EventItem(props) {
                 <div className={classes.summary}>
                     <h2>{title}</h2>
                     <div className={classes.date}>
+                        <DateIcon />
                         <time>{humanReadableDate}</time>
                     </div>
                     <div className={classes.address}>
+                        <AddressIcon />
                         <address>{formattedAddress}</address>
                     </div>
                 </div>
                 <div className={classes.actions}>
-                    <Link href={explorLink}>Explor Event</Link>
+                    {/* <Link href={explorLink}>Explor Event</Link> */}
+                    <Button link={explorLink}>
+                        <span>Explore Event</span>
+                        <span className={classes.icon}><ArrowRightIcon /></span>
+                    </Button>
+                    
                 </div>
             </div>
         </li>
