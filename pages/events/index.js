391052bed2ede51319da09830c1f35b7dfd1f848
Leslie Chou <chouyuchien@gmail.com>
2023-10-12 21:27:35 -0700

    added programmatical navigation to the filtered event page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,21 @@
+import { useRouter } from "next/router";
+
 import { getAllEvents } from "../../dummy.data";
 import EventList from '../../components/events/event-list';
 import EventsSearch from "../../components/events/events-search";
 
 function AllEventsPage() {
-
+    const router = useRouter();
     const events = getAllEvents();
+
+    function findEventsHander(year, month) {
+        const fullpath = `/events/${year}/${month}`;
+        router.push(fullpath);
+    }
 
     return (
         <>
-            <EventsSearch />
+            <EventsSearch onSearch={findEventsHander}/>
             <EventList items={events} />
         </>
     )
