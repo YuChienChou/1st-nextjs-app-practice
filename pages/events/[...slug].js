d2bd755fa8ef9472d08ecc1b1aa1070a60259901
Leslie Chou <chouyuchien@gmail.com>
2023-10-29 19:01:28 -0700

    added prerender function in slug page to prerender on the fly from the serverside

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,35 +1,30 @@
 import { useRouter } from 'next/router';
-import { getFilteredEvents } from '../../dummy.data';
+import { getFilteredEvents } from '../../components/helpers/api-util';
 import EventList from '../../components/events/event-list';
 import ResultsTitle from '../../components/events/results-title';
 import Button from '../../components/ui/button';
 import ErrorAlert from '../../components/ui/error-alert';
 
-function FilteredEventsPage() {
+function FilteredEventsPage(props) {
 
     const router = useRouter();
 
-    const filteredData = router.query.slug;
+    // const filteredData = router.query.slug;
 
-    if(!filteredData) {
-        return (
-            <p className='center'>Loading...</p>
-        )
-    }
+    // if(!filteredData) {
+    //     return (
+    //         <p className='center'>Loading...</p>
+    //     )
+    // }
 
-    const filteredYear = filteredData[0];
-    const filteredMonth = filteredData[1];
+    // const filteredYear = filteredData[0];
+    // const filteredMonth = filteredData[1];
     
-    const numYear = +filteredYear;
-    const numMonth = +filteredMonth;
+    // const numYear = +filteredYear;
+    // const numMonth = +filteredMonth;
 
     if(
-        isNaN(numYear) ||
-        isNaN(numMonth) ||
-        numYear > 2030 ||
-        numYear < 2021 ||
-        numMonth < 1 ||
-        numMonth > 12
+        props.hasError
     ) {
         return (
             <>
@@ -42,10 +37,7 @@ function FilteredEventsPage() {
         )
     }
 
-    const filteredEvents = getFilteredEvents({
-        year: numYear,
-        month: numMonth,
-    }) 
+    const filteredEvents = props.events;
     
     if(!filteredEvents || filteredEvents.length === 0) {
         return (
@@ -58,7 +50,7 @@ function FilteredEventsPage() {
         )
     }
 
-    const date = new Date(numYear, numMonth - 1);
+    const date = new Date(props.date.year, props.date.month - 1);
 
     return (
         <>
@@ -66,6 +58,56 @@ function FilteredEventsPage() {
             <EventList items={filteredEvents} />
         </>
     )
+}
+
+export async function getServerSideProps(context) {
+
+    const { params } = context;
+    console.log('params in slug page: ', params);
+
+    const filterData = params.slug;
+    console.log('filterData in slug page: ', filterData);
+
+    const filteredYear = filterData[0];
+    const filteredMonth = filterData[1];
+    
+    const numYear = +filteredYear;
+    const numMonth = +filteredMonth;
+
+    if(
+        isNaN(numYear) ||
+        isNaN(numMonth) ||
+        numYear > 2030 ||
+        numYear < 2021 ||
+        numMonth < 1 ||
+        numMonth > 12
+    ) {
+        return {
+            props: {
+                hasError: true
+            }
+            // notFound: true,  //this will show 404 page
+            // redirect: {
+            //     destination: '/error'
+            // }
+        }
+    }
+
+    const filteredEvents = await getFilteredEvents({
+        year: numYear,
+        month: numMonth,
+    });
+
+    return {
+        props: {
+            events: filteredEvents,
+            date: {
+                year: numYear,
+                month: numMonth,
+            }
+        }
+    }
+
 }
 
 
