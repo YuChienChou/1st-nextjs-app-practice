355ae882269b4548c8ae45904a8557594563706a
Leslie Chou <chouyuchien@gmail.com>
2023-10-30 21:41:05 -0700

    added _document file to set up HTML structure.

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <title>Next.js Events</title> // this will be overwritten if new Head title is defined in other pages
+        <meta name='description' content='Next.js Events'/> // this will be overwritten if new meta data is defined in other pages
         <meta name='viewport' content='inital-scale=1.0, width=devide-width' />
       </Head>
       <Component {...pageProps} />
